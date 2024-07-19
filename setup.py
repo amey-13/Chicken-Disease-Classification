aa9eb6fe87f9170b36b899594fb03d4de8af9fcb
amey-13 <ameysawant1300@gmail.com>
2024-07-18 19:39:08 -0500

    Data Ingestion

--- setup.py
+++ setup.py
@@ -6,22 +6,23 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = '0.0.0'
 
-REPO_NAME = 'Chicken-Disease-Classifier'
-AUTH_USERNAME = "amey-13"
-SRC_REPO = "ChickenDiseaseClassifier"
-AUTH_EMAIL = "ameysawant1300@gmail.com"
+REPO_NAME = 'Chicken-Disease-Classification'
+AUTH_USER_NAME = 'amey-13'
+SRC_REPO = 'ChickenDiseaseClasifier'
+AUTH_EMAIL = 'ameysawant1300@gmail.com'
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author=AUTH_USERNAME,
+    author=AUTH_USER_NAME,
     author_email=AUTH_EMAIL,
     description='A small python package for CNN app',
     long_description=long_description,
-    long_description_content="text/markdown",
-    url=f'https://github.com/{AUTH_USERNAME}/{REPO_NAME}',
+    long_description_content='text/markdown',
+    url=f'https://github.com/{AUTH_USER_NAME}/{REPO_NAME}',
     project_urls={
-        'Bug Tracker': f'https://github.com/{AUTH_USERNAME}/{REPO_NAME}/issues',
+        'Bug Tracker': f'https://github.com/{AUTH_USER_NAME}/{REPO_NAME}/issues',
     },
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src')
